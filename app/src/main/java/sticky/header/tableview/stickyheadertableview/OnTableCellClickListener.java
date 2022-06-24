cd730f5d91484abce6e9ab9c17a0dd3e232c744f
MitulVarmora <mpvarmora@gmail.com>
2022-06-24 15:45:14 +0530

    Gradle version updated. removed suppress annotations. minor code arrangement.

--- app/src/main/java/sticky/header/tableview/stickyheadertableview/OnTableCellClickListener.java
+++ app/src/main/java/sticky/header/tableview/stickyheadertableview/OnTableCellClickListener.java
@@ -2,6 +2,5 @@ package sticky.header.tableview.stickyheadertableview;
 
 public interface OnTableCellClickListener {
 
-    @SuppressWarnings("UnnecessaryInterfaceModifier")
-    public void onTableCellClicked(int rowPosition, int columnPosition);
+    void onTableCellClicked(int rowPosition, int columnPosition);
 }
